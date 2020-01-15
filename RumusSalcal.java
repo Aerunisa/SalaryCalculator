d81ade7cf0b03c4b39fff896a628ffd09d6531c8
Aerunisa <fitrianichaerunisaun@gmail.com>
2020-01-15 13:54:27 +0700

    Salary Calculator 3

--- RumusSalcal.java
+++ RumusSalcal.java
@@ -18,18 +18,21 @@ public class RumusSalcal{
 	    netto = gaji1 - pengurang;
 	    pph21 = netto + tunjangan;
 	    pphtotal = (gaji1 + tunjangan) - pph21;
-
+		System.out.print("Gaji Kotor :  \n");
+		System.out.print(pphtotal + " \n");
 	   
 	    //bpjs
 	    bpjs = ((gaji1 + tunjangan) * 4)/100;
-
+		System.out.print("Biaya BPJS : \n");
+		System.out.print(bpjs + " \n");
 	  
 	    //net salary
 	    total = pphtotal + bpjs;
-	    total2 = (gaji1 + tunjangan) - total;
-	      System.out.print(pphtotal + " \n");
-	        System.out.print(bpjs + " \n");
-	          System.out.print(total2 + " \n");
+		total2 = (gaji1 + tunjangan) - total;
+		System.out.print("Total Gaji Bersih : \n");
+		System.out.print(total2 + " \n");
+	        
+	          
 
 	}
 }
